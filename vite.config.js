7d2e457f81b306884173fa1e46bae810a847a72e
Zeizvant <oxanashvilizezva@gmail.com>
2025-03-21 03:32:57 +0400

    add Github Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Zeizvant/momentum',
   plugins: [
       react(),
       tailwindcss(),
