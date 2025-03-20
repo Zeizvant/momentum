1b4e3425044428c5f3377b322de38690fcaf76b4
Zeizvant <oxanashvilizezva@gmail.com>
2025-03-21 03:35:49 +0400

    change base option for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Zeizvant/momentum',
+  base: '/momentum/',
   plugins: [
       react(),
       tailwindcss(),
